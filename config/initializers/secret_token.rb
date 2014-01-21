da607e01f82e28a5958e0ad907d29aa9273f609a
Sue White <sue@mailworks.org>
2014-01-21 14:44:21 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeePeeps::Application.config.secret_key_base = '4fa6dcaf301ddfa3e69260f52a37fd638a52c2454d27545d372cbc841a0ddb766b194e8a1c5591b94e0148f1d3da249bb74bc327c752163c58e3c77a0cbdbb9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeePeeps::Application.config.session_store :cookie_store, key: '_SeePeeps_session'
