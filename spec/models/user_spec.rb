86dc91484170ac33cbb7a29c5976912a01094386
Galina <yermichevagalina@gmail.com>
2014-01-21 23:24:34 -0800

    creates user database

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe User do
+  let(:user) { User.new( email: "a@b.com", username: "Bookis", uid: "1234", provider: "twitter") }
+  pending "add some examples to (or delete) #{__FILE__}"
+end
