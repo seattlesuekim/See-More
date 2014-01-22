43928ec16a77384671ff1c5a30d422b259c4ab6a
Galina <yermichevagalina@gmail.com>
2014-01-21 23:28:47 -0800

    adds rspec tests for user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,10 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    raise 
-  new
+  end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice:"You have logged out!"
   end
-
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,43 @@ require 'spec_helper'
 
 describe User do
   let(:user) { User.new( email: "a@b.com", username: "Bookis", uid: "1234", provider: "twitter") }
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validations" do
+    it "is valid" do
+      expect(user).to be_valid
+    end
+    it "requires an email" do
+      user.email = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a username" do
+      user.username = nil
+      expect(user).to be_invalid
+    end
+
+    it "requires a uid" do
+      user.uid = nil
+      expect(user).to be_invalid
+    end
+    it "requires a provider" do
+      user.provider = nil
+      expect(user).to be_invalid
+    end
+  end
+
+  describe ".initialize_from_omniauth" do
+    let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
+
+    it "creates a valid user" do
+      expect(user).to be_valid
+    end
+
+    context "when it's invalid" do
+      it "returns nil" do
+        user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
+        expect(user).to be_nil
+      end
+    end
+  end
 end
