26ea96db73253fecbeb20922142f3840a7f91c46
Galina <yermichevagalina@gmail.com>
2014-01-22 14:22:10 -0800

    creates database for posts

---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+# Twitter.configure do |config|
+#   config.consumer_key = ENV["TWITTER_CLIENT_ID"]
+#   config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
+#   config.oauth_token = 'your_oauth_token_here'
+#   config.oauth_token_secret = 'your_oauth_token_secret_here'
+# end
