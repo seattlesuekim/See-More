f1d4b778410746a1fd5888f689508dca9e1a4469
Galina <yermichevagalina@gmail.com>
2014-01-22 22:43:46 -0800

    adds twitter search by username

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,5 @@
 # end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer 
-  # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SeePeeps::Application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   post "/auth/:provider/callback", to: "sessions#create"
   get "/signout", to: "sessions#destroy", as: :sign_out
+  post '/search', to: 'posts#search', as: :search
 
   resources :posts
   resources :users
