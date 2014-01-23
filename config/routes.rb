239a146c6408aedaa51692f1e555c21820b14ad0
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-23 09:09:37 -0800

    route conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ SeePeeps::Application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   post "/auth/:provider/callback", to: "sessions#create"
   get "/signout", to: "sessions#destroy", as: :sign_out
-<<<<<<< HEAD
+
   # get '/users/:id', to: "users#show"
-=======
+
   post '/search', to: 'posts#search', as: :search
->>>>>>> master
+
 
   resources :posts
   resources :users
