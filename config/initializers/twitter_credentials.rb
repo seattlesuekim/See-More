ea0c3372fd29dea7dcc931123b57ca38cd2091b8
Galina <yermichevagalina@gmail.com>
2014-01-22 18:05:01 -0800

    twitter gem configuration

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,8 +1,1 @@
 require 'twitter'
-
-# Twitter.configure do |config|
-#   config.consumer_key = ENV["TWITTER_CLIENT_ID"]
-#   config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
-#   config.oauth_token = 'your_oauth_token_here'
-#   config.oauth_token_secret = 'your_oauth_token_secret_here'
-# end
