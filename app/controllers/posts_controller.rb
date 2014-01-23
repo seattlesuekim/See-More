35b062436ceac739697f7ad99de4c62be74e6ada
Galina <yermichevagalina@gmail.com>
2014-01-22 18:05:01 -0800

    twitter gem configuration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_twitter_client
+
+  def create
+  end
+
+  def index
+
+  end
+
+  private
+  def set_tweets
+    # @tweets = client.user_timeline(<author>, :count =>10).collect  
+  end
+
+  def set_twits_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV["TWITTER_CLIENT_ID"]
+      config.consumer_secret = ENV["TWITTER_CLIENT_SECRET"]
+    end
+  end
 end
