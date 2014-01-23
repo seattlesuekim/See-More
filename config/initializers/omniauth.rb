352e7a411f056f97d4bf35cbb40d8a7538cd8586
Galina <yermichevagalina@gmail.com>
2014-01-22 23:15:42 -0800

    changed omniath config back to :developer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,6 @@
 # end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :developer 
+  # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
