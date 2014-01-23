c116a741d080efec6ce730d4c892173329614b90
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-23 12:24:18 -0800

    styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ActiveRecord::Base
   has_many :providers
 
   def self.find_or_create_from_omniauth(auth_hash)
-    User.find_by(email: auth_hash["info"]["email"]) || create_from_omniauth(auth_hash)
+    User.find_by(email: auth_hash["info"]["email"], username: auth_hash['info']['name']) || create_from_omniauth(auth_hash)
   end
 
   def self.create_from_omniauth(auth_hash)
     self.create!(      
-      username:    auth_hash["info"]["name"],
-      email: auth_hash["info"]["email"],
+      username: auth_hash["info"]["name"],
+      email:    auth_hash["info"]["email"],
     )
   rescue ActiveRecord::RecordInvalid
     nil
