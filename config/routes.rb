6e04adff0c1ff059db315678a8846b5382513e67
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-22 23:25:37 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SeePeeps::Application.routes.draw do
   post "/auth/:provider/callback", to: "sessions#create"
 
   get "/signout", to: "sessions#destroy", as: :sign_out
+  # get '/users/:id', to: "users#show"
 
   root to: "users#show" 
   
