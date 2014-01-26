099804dae9570823d2dea0b8125f7f867da103a2
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-01-25 20:04:40 -0800

    Tumblr save works

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,8 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:delete]
 
-
   def create
+
     @author   = Author.find_by(uid: params[:author][:uid])
     @author ||= current_user.authors.build( author_params)
     begin
@@ -33,4 +33,3 @@ class AuthorsController < ApplicationController
   end
 
 end
-#{}
