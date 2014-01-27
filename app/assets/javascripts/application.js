3f519bccbb6037015a53a8e3882b46ae0ebaccab
Galina <yermichevagalina@gmail.com>
2014-01-27 12:36:04 -0800

    adds javascript to user's page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(function(){
+  $('#profiletabs ul li a').on('click', function(e){
+    e.preventDefault();
+    var newcontent = $(this).attr('href');
+     
+    $('#profiletabs ul li a').removeClass('sel');
+    $(this).addClass('sel');
+     
+    $('#content section').each(function(){
+      if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+    });
+     
+    $(newcontent).removeClass('hidden');
+  });
+});
