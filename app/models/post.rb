208fa65eed933631b7fd90af4eacf3aee8a37931
Sue White <sue@mailworks.org>
2014-01-28 14:05:43 -0800

    author gets instantiated, but incorrectly

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,23 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  attr_accessor :feed
 
-  attr_accessor :feed, :post
-
-  #TODO: CONNECT TO AUTHOR
   # RSS feed from other blogs
+  # Sometimes the Author isn't available; it's nil. What to do?
   def self.from_rss(url)
     @feed = Feedzirra::Feed.fetch_and_parse(url)
     if not @feed.is_a?(Fixnum) && @feed != nil
-    #if @feed.is_a?(Feedzirra::Parser::RSS) || @feed.is_a?(Feedzirra::Parser::Atom)
       entry = @feed.entries.first
+      author = Author.new
+      author.username = entry.author || @feed.title
       @post = Post.new do |p|
-        p.author_id = entry.author
+        p.author_id = (Author.find_by username: author.username).id
         p.body = entry.content
         p.title = entry.title
-        #p.posted_at = entry.published
+        p.posted_at = entry.published
         # p.created_at = Automatically gets set to the current date and time when the record is first created.
       end
+      raise()
       @post.save
       @post
     end
