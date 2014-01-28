64a7cfb310a65d278904853fa9b8ae18fee5cd17
Galina <yermichevagalina@gmail.com>
2014-01-28 11:21:07 -0800

    added secret and token columns to providers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128191826) do
+ActiveRecord::Schema.define(version: 20140128192008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20140128191826) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "token"
+    t.string   "secret"
   end
 
   create_table "user_authors", force: true do |t|
