6d9de86c993b7f2abf786de10cb28aa6f0464e7b
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-01-29 16:57:24 -0800

    New Nav Bar with Searches

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,27 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 // require turbolinks
 //= require_tree .
 
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
+
+if ($(window).width() >= 780) {
+    jQuery('ul.nav li.dropdown').hover(function () {
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn();
+    }, function () {
+        jQuery(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut();
+    });
+}
 
 $(function(){
   $('#profiletabs ul li a').on('click', function(e){
@@ -42,3 +60,4 @@ function CheckMaxlength(oInObj)
           oInObj.value = oInObj.value.substring(0, iMaxLen);
       }
 }
+
