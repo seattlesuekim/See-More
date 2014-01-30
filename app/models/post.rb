aa79d9e3b8e8136f9a17edc9443f78388f77f5ea
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-30 11:32:48 -0800

    refactor post association syntax for tumblrauthor

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,21 +7,21 @@ class Post < ActiveRecord::Base
     author = Author.find_by(uid: post['blog_name'])
     case post['type']
       when 'video'
-        author.posts << self.create(body: post["player"].first["embed_code"], posted_at: post["date"])
+        author.posts.create(body: post["player"].first["embed_code"], posted_at: post["date"])
       when 'text'
-        author.posts << self.create(body: post["body"], posted_at: post["date"])
+        author.posts.create(body: post["body"], posted_at: post["date"])
       when 'quote'
-        author.posts << self.create(body: "#{post["text"]} #{post["source"]}", posted_at: post["date"])
+        author.posts.create(body: "#{post["text"]} #{post["source"]}", posted_at: post["date"])
       when 'chat'
         chat = post['body'].gsub(/\r\n/, '<br>')
-        author.posts << self.create(body: chat, posted_at: post["date"])
+        author.posts.create(body: chat, posted_at: post["date"])
       when 'answer'
-        author.posts << self.create(body: post["body"], posted_at: post["date"])
+        author.posts.create(body: post["body"], posted_at: post["date"])
       when 'photo'
         photoset = post['photos'].map {|photo| "<img src= '#{photo['original_size']['url']}', width= '450'>"}.join("")
-        author.posts << self.create(body: photoset, posted_at: post["date"])
+        author.posts.create(body: photoset, posted_at: post["date"])
       when 'audio'
-        author.posts << self.create(body: post["player"], posted_at: post["date"])
+        author.posts.create(body: post["player"], posted_at: post["date"])
     end
   end
 end
