8b1bf19269ed9fc2c9e07fc5a4d45fd42fe2ef37
Galina <yermichevagalina@gmail.com>
2014-01-31 11:12:55 -0800

    added links to the user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
 
   def render_post(post)
     if post[:author_type] == "TwitterAuthor"
-      @rendered = post[:body].gsub(/(http:\/\/[a-zA-Z0-9\/\.\+\-_:?&=]+)/) {|a| "<a href=\"#{a}\" target='_blank'>#{a}</a>"}  
+      @rendered = post[:body].gsub(/(http:\/\/[a-zA-Z0-9\/\.\+\-_:?&=]+)/) {|a| "<a href=\"#{a}\" target='_blank'>#{a}</a>"}
     elsif post[:author_type] == "TumblrAuthor"
       @rendered = post[:body]
     end
@@ -30,6 +30,28 @@ module ApplicationHelper
   def has_twitter_provider
     types = current_user.providers.map {|p| p.name}
     types.include? "twitter"
+  end
+
+  def img_linkify(p)
+   case p[:author_type]
+   when "TwitterAuthor"
+    link_to image_tag("#{p[:author_url]}", :class=> "img-thumbnail", :size=> "80x80"), "https://twitter.com/#{p[:author_name]}", target: "_blank"
+   when "TumblrAuthor"
+    link_to image_tag("#{p[:author_url]}", :class=> "img-thumbnail", :size=> "80x80"), "http://#{p[:author_name]}.tumblr.com", target: "_blank"
+   else
+    image_tag("#{p[:author_url]}", :class=> "img-thumbnail", :size=> "80x80")
+   end
+  end
+
+  def txt_linkify(p)
+   case p[:author_type]
+   when "TwitterAuthor"
+    link_to p[:author_name], "https://twitter.com/#{p[:author_name]}", target: "_blank"
+   when "TumblrAuthor"
+    link_to p[:author_name], "http://#{p[:author_name]}.tumblr.com", target: "_blank"
+   else
+    p[:author_name]
+   end
   end
 
 end
