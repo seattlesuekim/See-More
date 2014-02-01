8938e6b035d03519253ae72e560c6f146b41f1b7
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-01-31 16:53:09 -0800

    added instagram omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CLIENT_ID'], ENV['TWITTER_CLIENT_SECRET']
-  provider :tumblr, ENV['TUMBLR_CLIENT_ID'], ENV['TUMBLR_CLIENT_SECRET']  
+  provider :tumblr, ENV['TUMBLR_CLIENT_ID'], ENV['TUMBLR_CLIENT_SECRET']
+  provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']  
 end
