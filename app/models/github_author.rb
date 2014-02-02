6c9808e8deb13179003d2b27773f3eb3dbbdf4b9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-01 23:41:31 -0800

    fix typo GITHUB

--- app/models/github_author.rb
+++ app/models/github_author.rb
@@ -1,6 +1,6 @@
 class GithubAuthor < Author
 
   def self.client
-    @client = Octokit::Client.new :client_id => ENV["GUTHUB_CLIENT_ID"], :client_secret => ENV["GITHUB_CLIENT_SECRET"]
+    @client = Octokit::Client.new :client_id => ENV["GITHUB_CLIENT_ID"], :client_secret => ENV["GITHUB_CLIENT_SECRET"]
   end
 end
